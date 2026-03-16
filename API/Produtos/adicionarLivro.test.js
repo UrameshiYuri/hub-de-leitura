c1083acfd415891290a42361704b3b385018f560
Yuri Mielli <yumielllli@gmail.com>
2026-03-16 20:19:05 +0100

    terminando a area de produtos

--- API/Produtos/adicionarLivro.test.js
+++ API/Produtos/adicionarLivro.test.js
@@ -1,32 +1,32 @@
-const { spec } = require('pactum')
+const { spec } = require('pactum');
 
-let token;
 beforeEach(async () => {
-    token = await spec()
+     await spec()
         .post('http://localhost:3000/api/login')
         .withJson({
             "email": "admin@biblioteca.com",
             "password": "admin123"
         })
-        .stores('token')
+        .stores('token', 'token_for_swagger');
 });
 
 it('Deve adicionar livro com sucesso', async () => {
     await spec()
         .post('http://localhost:3000/api/books')
+        .withHeaders('Authorization', `Bearer $S{token}`)
         .withJson({
-            "title": "livro de teste com vscode",
-            "author": "video de teste",
-            "description": "teste",
-            "category": "Literatura Brasileira",
-            "isbn": "111111111111111",
+            "title": "livro a ser deletado com vscode",
+            "author": "livro a ser deletado",
+            "description": "deletado",
+            "category": "delet",
+            "isbn": "404-NOT FOUND",
             "editor": "test",
             "language": "Português",
             "publication_year": 2026,
             "pages": 312,
             "format": "Físico",
-            "total_copies": 4,
-            "available_copies": 4
+            "total_copies": 5,
+            "available_copies": 5
         })
         .expectStatus(201)
         .expectJson('message', 'Livro criado com sucesso.');
